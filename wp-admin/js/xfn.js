e4c2fe9dff9ab2e30d4ccd845d026aeba1edbce7
Susana Piñero <susananzth@gmail.com>
2021-03-11 23:39:48 -0500

    Actualizando wp a 5.7. dejando base de cero, subiendo theme de susananzth

--- wp-admin/js/xfn.js
+++ wp-admin/js/xfn.js
@@ -1,6 +1,12 @@
+/**
+ * Generates the XHTML Friends Network 'rel' string from the inputs.
+ *
+ * @deprecated 3.5.0
+ * @output wp-admin/js/xfn.js
+ */
 jQuery( document ).ready(function( $ ) {
 	$( '#link_rel' ).prop( 'readonly', true );
-	$( '#linkxfndiv input' ).bind( 'click keyup', function() {
+	$( '#linkxfndiv input' ).on( 'click keyup', function() {
 		var isMe = $( '#me' ).is( ':checked' ), inputs = '';
 		$( 'input.valinp' ).each( function() {
 			if ( isMe ) {
---
+++ wp-content/themes/susananzth/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', "");
+    define('DB_NAME', 'wp-store');
+    define('DB_CHARSET', 'utf8mb4_general_Ci');
+?>
